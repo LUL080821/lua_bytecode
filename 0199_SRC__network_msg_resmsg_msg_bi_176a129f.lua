10d2c04737c06396b5250d1bb42f763411318008
LUL080821 <gutenappetit911@gmail.com>
2026-02-09 10:23:57 +0700

    fin

--- 0199_SRC__network_msg_resmsg_msg_bi_176a129f.lua
+++
@@ -1,7 +0,0 @@
-local MSG_BI = {}
-local Network = GameCenter.Network
-
-function MSG_BI.RegisterMsg()
-end
-return MSG_BI
-
--- 0208_SRC__network_msg_resmsg_msg_common_70041deb.lua
+++
@@ -1,7 +0,0 @@
-local MSG_Common = {}
-local Network = GameCenter.Network
-
-function MSG_Common.RegisterMsg()
-end
-return MSG_Common
-
